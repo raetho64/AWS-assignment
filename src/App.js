ba9b648209d86b476f114667201112290770b3ea
raetho64 <raekwonnthomas@gmail.com>
2024-06-17 07:24:48 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        CS 230L
+      </h1>
+      <h2>
+        Section - 02
+      </h2>
+      <p>
+        WVU ID: 800379550
+      </p>
+      <p>
+        Hi I am Raekwon Thomas
+      </p>
+    </div>
+  );
+}
+
+export default App;
